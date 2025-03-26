27a1bb33d18c583199fcadab5d14c9ac9bb58d82
faaz <mdfaazkhan786@gmail.com>
2025-03-27 04:37:12 +0530

    sjkerhglers

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -16,6 +16,7 @@ const ChatPage = () => {
     <div> {chats.map((chat) =>(
       <div key={chat._id}>{chat.chatName} </div>))}
       <div>hello my name is faaz</div>
+      <div>ok again i did this to check another div is that ok</div>
     </div>
   );
 }
